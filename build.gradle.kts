a1d2777728e81782af072d774a36c2ab3dab79ca
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2024-04-28 12:54:34 +0200

    now?

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     api(libs.org.jline.jline.reader)
     api(libs.org.ow2.asm.asm)
     testImplementation(libs.junit.junit)
-    compileOnly(libs.org.projectlombok.lombok)
+    api(libs.org.projectlombok.lombok)
 }
 
 group = "com.bedrockcloud"
