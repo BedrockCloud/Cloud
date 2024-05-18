75f84510ab82eb748556bb4ce6f6ea28ff506d85
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2024-05-18 18:08:42 +0200

    update PlayerMessagePacket.java

--- src/main/java/com/bedrockcloud/bedrockcloud/network/packets/PlayerMessagePacket.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/network/packets/PlayerMessagePacket.java
@@ -8,26 +8,30 @@ import org.json.simple.JSONObject;
 
 public class PlayerMessagePacket extends DataPacket
 {
-    public String playerName;
+    public String players;
     public String value;
     
     @Override
     public void handle(final JSONObject jsonObject, final ClientRequest clientRequest) {
-        final String playerName = jsonObject.get("playerName").toString();
+        final String players = jsonObject.get("players").toString();
         final String value = jsonObject.get("value").toString();
-        if (Cloud.getCloudPlayerProvider().existsPlayer(playerName)) {
-            final CloudPlayer cloudPlayer = Cloud.getCloudPlayerProvider().getCloudPlayer(playerName);
-            final PlayerTextPacket playerTextPacket = new PlayerTextPacket();
-            playerTextPacket.playerName = cloudPlayer.getPlayerName();
-            playerTextPacket.type = 0;
-            playerTextPacket.value = value;
-            cloudPlayer.getProxy().pushPacket(playerTextPacket);
+        String[] playerArray = players.split(":");
+
+        for (String player : playerArray) {
+            if (Cloud.getCloudPlayerProvider().existsPlayer(player)) {
+                final CloudPlayer cloudPlayer = Cloud.getCloudPlayerProvider().getCloudPlayer(player);
+                final PlayerTextPacket playerTextPacket = new PlayerTextPacket();
+                playerTextPacket.playerName = cloudPlayer.getPlayerName();
+                playerTextPacket.type = 0;
+                playerTextPacket.value = value;
+                cloudPlayer.getProxy().pushPacket(playerTextPacket);
+            }
         }
     }
     
     @Override
     public String encode() {
-        this.addValue("playerName", this.playerName);
+        this.addValue("players", this.players);
         this.addValue("type", 0);
         this.addValue("value", this.value);
         return super.encode();
