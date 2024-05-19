ebd5eee131a8ec89a833414c77871d351a12d268
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2024-05-19 21:18:38 +0200

    updated packets

--- src/main/java/com/bedrockcloud/bedrockcloud/network/packets/PlayerMessagePacket.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/network/packets/PlayerMessagePacket.java
@@ -15,6 +15,20 @@ public class PlayerMessagePacket extends DataPacket
     public void handle(final JSONObject jsonObject, final ClientRequest clientRequest) {
         final String players = jsonObject.get("players").toString();
         final String value = jsonObject.get("value").toString();
+
+        if (players.equalsIgnoreCase("ALL")) {
+            for (CloudPlayer cloudPlayer : Cloud.getCloudPlayerProvider().getCloudPlayerMap().values()) {
+                if (Cloud.getCloudPlayerProvider().existsPlayer(cloudPlayer.getPlayerName())) {
+                    final PlayerTextPacket playerTextPacket = new PlayerTextPacket();
+                    playerTextPacket.playerName = cloudPlayer.getPlayerName();
+                    playerTextPacket.type = 0;
+                    playerTextPacket.value = value;
+                    cloudPlayer.getProxy().pushPacket(playerTextPacket);
+                }
+            }
+            return;
+        }
+
         String[] playerArray = players.split(":");
 
         for (String player : playerArray) {
