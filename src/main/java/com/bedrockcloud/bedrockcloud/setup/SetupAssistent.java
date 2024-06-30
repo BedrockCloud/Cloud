f187da5be0d6c99c0371a2e5548254938dcfeef0
Florian <38558911+WhitqFlorian@users.noreply.github.com>
2024-06-30 23:16:48 +0200

    create config file after finishing setup

--- src/main/java/com/bedrockcloud/bedrockcloud/setup/SetupAssistent.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/setup/SetupAssistent.java
@@ -1,10 +1,13 @@
 package com.bedrockcloud.bedrockcloud.setup;
 
+import com.bedrockcloud.bedrockcloud.utils.Utils;
+import com.bedrockcloud.bedrockcloud.utils.config.Config;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SetupAssistent {
@@ -65,6 +68,45 @@ public class SetupAssistent {
                     System.out.println("Invalid input. Please enter 'true' or 'false'.");
                 }
             }
+
+            // Enable debug mode
+            boolean loginExtras = false;
+            while (true) {
+                String extrasInput = reader.readLine("Enable Waterdog login extras (true/false): ");
+                if ("true".equalsIgnoreCase(extrasInput)) {
+                    loginExtras = true;
+                    break;
+                } else if ("false".equalsIgnoreCase(extrasInput)) {
+                    loginExtras = false;
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'true' or 'false'.");
+                }
+            }
+
+            File localConfigFile = new File("./local/config.yml");
+
+            if (!localConfigFile.exists()) {
+                try {
+                    localConfigFile.createNewFile();
+                } catch (IOException ignored) {
+                }
+            }
+
+            Config config = new Config(localConfigFile, Config.YAML);
+
+            if (!config.exists("port")) config.set("port", (double)defaultCloudPort);
+            if (!config.exists("debug-mode")) config.set("debug-mode", enableDebugMode);
+            if (!config.exists("motd")) config.set("motd", "Default Cloud Service");
+            if (!config.exists("auto-update-on-start")) config.set("auto-update-on-start", false);
+            if (!config.exists("wdpe-login-extras")) config.set("wdpe-login-extras", loginExtras);
+            if (!config.exists("enable-log")) config.set("enable-log", true);
+            if (!config.exists("start-method")) config.set("start-method", "tmux");
+            if (!config.exists("rest-password")) config.set("rest-password", Utils.generateRandomPassword(8));
+            if (!config.exists("rest-port")) config.set("rest-port", 8080.0);
+            if (!config.exists("rest-username")) config.set("rest-username", "cloud");
+            if (!config.exists("rest-enabled")) config.set("rest-enabled", enableRestapi);
+            if (!config.exists("service-separator")) config.set("service-separator", "-");
         } catch (IOException ignored) {}
     }
 }
